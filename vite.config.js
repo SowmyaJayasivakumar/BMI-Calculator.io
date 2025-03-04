65358dcc50fbb648e2142cd7d1e60d6db6298f4a
SowmyaJayasivakumar <sowmyajayasivakumar@gmail.com>
2025-03-05 01:04:04 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  base: '/bmicalc/', // Change this to match your GitHub repository name
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // This is the folder where the build files will be generated
+  },
+});
