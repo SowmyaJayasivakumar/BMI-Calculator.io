560cd67009073a1f585424f8247e363ff21b82df
SowmyaJayasivakumar <sowmyajayasivakumar@gmail.com>
2025-03-05 11:19:51 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/BMI-Calculator/', // Change this to match your GitHub repository name
+  base: '/BMI-Calculator.io/', // Change this to match your GitHub repository name
   plugins: [react()],
   build: {
     outDir: 'dist', // This is the folder where the build files will be generated
